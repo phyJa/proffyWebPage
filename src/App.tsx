033a40b87e32b7f7cff78b96f39a9d680cd2005f
Elias <eli.bueno9@gmail.com>
2020-08-03 12:34:19 -0300

    Working in CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+//Note that this style is inside the JS
+import './assets/styles/global.css';
 
 function App() {
   return (
     <div className="App">
       <h1> Hello world! </h1>
+      <button>Some button</button>
     </div>
   );
 }
