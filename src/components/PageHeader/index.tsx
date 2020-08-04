fd68deac4cd515eb2693f8626246a3a0289a24fe
Elias <eli.bueno9@gmail.com>
2020-08-04 11:35:04 -0300

    Starting to add teacher images

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -21,21 +21,21 @@ interface PageHeaderProps {
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
     return (
         <header className="page-header">
-                <div className="top-bar-container">
-                    <Link to="/">
-                        <img src={backIcon} alt="Return to home page" />
-                    </Link>
-                    <img src={logoImg} alt="Proffy"/>
-                </div>
+            <div className="top-bar-container">
+                <Link to="/">
+                    <img src={backIcon} alt="Return to home page" />
+                </Link>
+                <img src={logoImg} alt="Proffy"/>
+            </div>
 
-                <div className="header-content">
-                    <strong>
-                        {props.title}
-                    </strong>
+            <div className="header-content">
+                <strong>
+                    {props.title}
+                </strong>
 
-                    {props.children}
-                </div>
-            </header>
+                {props.children}
+            </div>
+        </header>
     );
 }
 
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
+//Images
+import whatsAppIcon from '../../assets/images/icons/whatsapp.svg';
+
 // CSS
 import './styles.css';  
 
@@ -26,6 +29,38 @@ export default function TeacherList () {
                     </div>
                 </form>
             </PageHeader>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://avatars0.githubusercontent.com/u/62406306?s=460&u=929ad34b0ed0d7adf4025777839101452b1dbe55&v=4" alt="Elias"/>  
+                        
+                        <div>
+                            <strong>Elias</strong>
+                            
+                            <span>Física</span>
+                        </div>
+                    </header>
+
+                    <p>
+                        Description Description  Description  Description  Description
+                        <br/> <br/>
+                        An undergraduate physics student. Likes to watch the sky.
+                    </p>
+
+                    <footer>
+                        <p>
+                            Pricing:
+                            <strong>R$ 40,00 per hour</strong>
+                        </p>
+
+                        <button type="button">
+                            <img src={whatsAppIcon} alt="Whatsapp"/>
+                            Contact
+                        </button>
+                    </footer>
+                </article>  
+            </main>
         </div>
     )
 }
