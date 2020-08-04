0ce3faf2fed30259c418c5b30fc1fe660d1a176e
Elias <eli.bueno9@gmail.com>
2020-08-03 21:54:33 -0300

    Header of Teacher List done

--- src/pages/teachers/index.tsx
+++ src/pages/teachers/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+// Images
+import logoImg from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg';
+
+// CSS
+import './styles.css';  
 
 export default function TeacherList () {
     return (
-        <h1>Teachers (test)</h1>
+        <div id="page-teacher-list" className="container">
+            <header className="page-header">
+                <div className="top-bar-container">
+                    <Link to="/">
+                        <img src={backIcon} alt="Return to home page" />
+                    </Link>
+                    <img src={logoImg} alt="Proffy"/>
+                </div>
+
+            <div className="header-content">
+                <strong>
+                    These are the available Proffys.
+                </strong>
+            </div>
+            </header>
+
+        </div>
     )
 }
