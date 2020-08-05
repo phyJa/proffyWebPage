9979b22111fcc252c13048fb857e3e856e44d173
Elias <eli.bueno9@gmail.com>
2020-08-05 20:53:57 -0300

    Starting to develop Teacher Form

--- src/pages/TeacherForm/TeacherForm.tsx
+++ src/pages/TeacherForm/TeacherForm.tsx
@@ -6,8 +6,29 @@ export default function TeacherForm() {
         <div id="page-teacher-form" className="container">
             <PageHeader 
                 title="Amazing! So you want to teach!"
-                description="The first step is to complete this form." 
+                description="The first step is to complete this submission form." 
             />
+
+            <main>
+                <fieldset>
+                    <legend>Your data</legend>
+
+                    <div className="input-block">
+                        <label htmlFor="name">Full name</label>
+                        <input type="text" id="name"/>
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="avatar">Avatar</label>
+                        <input type="text" id="avatar"/>
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="whatsapp">Whatsapp</label>
+                        <input type="text" id="whatsapp"/>
+                    </div>
+                </fieldset>
+            </main>
         </div>
     )
 }
