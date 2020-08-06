f008076af8e47e019904e492a91aabdb3d4bb2e1
Elias <eli.bueno9@gmail.com>
2020-08-06 19:03:55 -0300

    First connection between Front and Back donegit add .

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 //This is for the browser not reload the entire page when clicking the links
 import {Link} from 'react-router-dom';
@@ -13,8 +13,29 @@ import landingImg from  '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+import api from '../../services/api';
 
 function Landing () {
+    //Let's use states once more, but at this time starting with the value 0
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    // Then, import { useEffect } from react. This is a function with two
+    // parameters: 
+    // 1. A function (the first)
+    // 2. An array of dependencies, which will state when to call the 
+    // first parameter. When this array has its values changed, it calls
+    // the first argument again. If you want to execute the function only once
+    // when the component is shown in screen, then keep the array empty.
+    useEffect(
+        () => {
+            api.get('/connections').then(response => {
+                    console.log(response.data);
+                }
+            );
+        },
+        []
+    );
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
