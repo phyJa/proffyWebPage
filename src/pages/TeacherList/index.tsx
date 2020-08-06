7ace097bf5cd3a3f5c00c11d0e1fabff3916361b
Elias <eli.bueno9@gmail.com>
2020-08-06 13:02:10 -0300

    Adding Select components to Teacher List

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,20 +2,49 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 // CSS
 import './styles.css';  
-
-
 
 export default function TeacherList () {
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="These are the available Proffys">
                 <form id="search-teachers">
-                    <Input name="subject" label="Subject"/>
+                    <Select 
+                            name="subject" 
+                            label="Subject"
+                            options={
+                                [
+                                    {value: 'Art', label: 'Art'},
+                                    {value: 'Biology', label: 'Biology'},
+                                    {value: 'Physic Education', label: 'Physic Education'},
+                                    {value: 'English', label: 'English'},
+                                    {value: 'Portuguese', label: 'Portuguese'},
+                                    {value: 'Physics', label: 'Physics'},
+                                    {value: 'Chemistry', label: 'Chemistry'},
+                                    {value: 'History', label: 'History'},
+                                    {value: 'Geography', label: 'Geography'}
+                                ]
+                            }
+                    />
 
-                    <Input name="week_day" label="Week Day"/>
+                    <Select 
+                        name="week_day" 
+                        label="Week Day"
+                        options={
+                            [
+                                {value: '0', label: 'Sunday'},
+                                {value: '1', label: 'Monday'},
+                                {value: '2', label: 'Tuesday'},
+                                {value: '3', label: 'Wednesday'},
+                                {value: '4', label: 'Thursday'},
+                                {value: '5', label: 'Friday'},
+                                {value: '6', label: 'Saturday'}
+                            ]
+                        }
+                    />
                     
                     <Input name="time" label="Time" type="time"/>
                 </form>
