0e675c5dbe027b9f9af3a0a9b6078b1443cc5483
Elias <eli.bueno9@gmail.com>
2020-08-06 08:57:49 -0300

    Moving Input styles from TeacherList to Input Component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {InputHTMLAttributes} from 'react';
 
-interface InputProps {
+import './styles.css';
+
+// Now we want to pass all the attributes that a normal HTML input
+// would receive at its standard. This is made by importing InputHTMLAttributes,
+// extending our interface and passing the global variable HTMLInputElement
+// in the argument:
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     label: string;
     name: string;
 }
 
-const Input: React.FC<InputProps> = ({ label, name /* Another way to use props by desctruturing */ }) => {
+
+// Use the spread operator to take all the other props from InputHtmlAttributes. Note they are all
+// optional
+const Input: React.FC<InputProps> = ({ label, name, ...rest  /* Another way to use props by desctruturing */ }) => {
     return (
         <div className="input-block">
-            <label htmlFor={name}>{ label }</label>
-            <input type="text" id={name}/>
+            <label htmlFor={name}>{label}</label>
+            <input type="text" id={name} {...rest} />
         </div>
     );
 }
